e0bff0d0146540dc2e2cff01bd6cc7a9fad72e47
Isabela Quintela Matos <iquintelam@gmail.com>
2020-03-11 12:17:37 -0400

    created main file

---
+++ main.py
@@ -0,0 +1,10 @@
+from math import sqrt, ceil, floor, pi, cos, sin
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+from scipy.fftpack import fft, ifft, rfft
+from scipy import integrate
+import sys
+
+print("wash your hands")
